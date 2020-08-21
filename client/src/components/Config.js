83f4c9c96ec9802cfa2e66daf3f8d3274a967af8
leensharma <leensharma12gmail.com>
2020-08-21 20:13:08 +0530

    error fixing

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,7 +1,7 @@
 //SERVER ROUTES
 export const USER_SERVER = "/api/users";
 
-export const API_KEY = `${process.env.REACT_APP_API_KEY}`;
+export const API_KEY = `4a7ccec2053bd7baa3a6670090df5ebe`;
 export const API_URL = "https://api.themoviedb.org/3/";
 export const IMAGE_URL = "https://image.tmdb.org/t/p/";
 
