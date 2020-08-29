d3ad9da0d8bf96a2152478a7c8db7cc71598f816
leensharma <leensharma12gmail.com>
2020-08-29 10:57:11 +0530

    fixed main image component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -18,7 +18,7 @@ function App() {
   return (
     <>
       <NavBar />
-      <div style={{ paddingTop: "55px" }}>
+      <div style={{ paddingTop: "60px" }}>
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route
--- client/src/components/views/LandingPage/Sections/MainImage.js
+++ client/src/components/views/LandingPage/Sections/MainImage.js
@@ -6,13 +6,9 @@ function MainImage(props) {
     <Link to={`/movie/${props.movieId}`}>
       <div
         style={{
-          background: `linear-gradient(to bottom, rgba(0,0,0,0)
-    39%,rgba(0,0,0,0)
-    41%,rgba(0,0,0,0.65)
-    100%),
-    url('${props.image}'), #1c1c1c`,
+          backgroundImage: `url("${props.image}")`,
           height: "600px",
-          backgroundSize: "100%,cover",
+          backgroundSize: "cover,cover",
           backgroundPosition: "center, center",
           width: "100%",
           position: "relative",
